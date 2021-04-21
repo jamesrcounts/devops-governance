0f66e9e4d4f346a437deb44f8b53c2e18b80a2cb
Jim Counts <jamesrcounts@outlook.com>
2021-04-21 13:03:18 +0000

    Add generated key

--- modules/azure-backend/key-vault.tf
+++ modules/azure-backend/key-vault.tf
@@ -1,3 +1,7 @@
+locals {
+  generated_keys = ["state-cmk-${local.project_id}"]
+}
+
 resource "azurerm_key_vault" "backend" {
   enable_rbac_authorization   = true
   enabled_for_disk_encryption = true
@@ -9,4 +13,23 @@ resource "azurerm_key_vault" "backend" {
   soft_delete_retention_days  = 7
   tags                        = local.tags
   tenant_id                   = data.azurerm_client_config.current.tenant_id
+}
+
+resource "azurerm_key_vault_key" "generated" {
+  for_each = toset(local.generated_keys)
+
+  name         = each.key
+  key_vault_id = azurerm_key_vault.backend.id
+  key_type     = "RSA"
+  key_size     = 4096
+  tags         = local.tags
+
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
 }
