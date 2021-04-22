57872fcc4ed39e458ddba8a1df879595feaf4239
Jim Counts <jamesrcounts@outlook.com>
2021-04-22 02:36:40 +0000

    Add container

---
+++ modules/azure-backend/stoarage-container.tf
@@ -0,0 +1,5 @@
+resource "azurerm_storage_container" "state" {
+  container_access_type = "private"
+  name                  = "state"
+  storage_account_name  = azurerm_storage_account.backend.name
+}
