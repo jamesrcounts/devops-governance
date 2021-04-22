8e15d1015d7566fe01ba0a5d0f813861d23d160c
Jim Counts <jamesrcounts@outlook.com>
2021-04-22 14:32:39 +0000

    Problems with the slash in the tag.

--- projects/root/azure-backend.tf
+++ projects/root/azure-backend.tf
@@ -1,9 +1,9 @@
 module "azure_backend" {
-  source = "github.com/jamesrcounts/devops-governance.git//modules/azure-backend?ref=azure-backend/0.0.1"
+  source = "github.com/jamesrcounts/devops-governance.git//modules/azure-backend?ref=azure-backend-0.0.1"
 
   location = "centralus"
   required_tags = {
+    project    = local.project
     repository = "jamesrcounts/devops-governance"
-    project    = "terraform-root"
   }
 }
