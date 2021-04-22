f80d75f9f26b0d249a5ae173049f5b7f1036b24a
Jim Counts <jamesrcounts@outlook.com>
2021-04-22 13:38:20 +0000

    Update ref to main

--- projects/root/azure-backend.tf
+++ projects/root/azure-backend.tf
@@ -1,5 +1,5 @@
 module "azure_backend" {
-  source = "github.com/jamesrcounts/devops-governance.git//modules/azure-backend?ref=bootstrap"
+  source = "github.com/jamesrcounts/devops-governance.git//modules/azure-backend?ref=main"
 
   location = "centralus"
   required_tags = {
