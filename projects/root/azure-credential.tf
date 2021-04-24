f298052668a1d96156e7f5e35782240bfb1ccbee
Jim Counts <jamesrcounts@outlook.com>
2021-04-24 22:27:35 +0000

    Add update triggers

--- projects/root/azure-credential.tf
+++ projects/root/azure-credential.tf
@@ -2,4 +2,8 @@ module "azure_credentials" {
   source = "github.com/jamesrcounts/devops-governance.git//modules/azure-credentials?ref=credentials"
 
   project = local.project
+  update_triggers = {
+    primary   = "20210424"
+    secondary = "20210424"
+  }
 }
