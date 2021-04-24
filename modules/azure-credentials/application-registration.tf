9fbdb0b37ba896a040583ba2e67d644633e5860c
Jim Counts <jamesrcounts@outlook.com>
2021-04-24 22:01:22 +0000

    Add service principal

--- modules/azure-credentials/application-registration.tf
+++ modules/azure-credentials/application-registration.tf
@@ -1,4 +1,4 @@
-resource "azuread_application" "application" {
+resource "azuread_application" "app" {
   available_to_other_tenants = false
   display_name               = "${var.project} (Managed by Terraform)"
   oauth2_allow_implicit_flow = false
