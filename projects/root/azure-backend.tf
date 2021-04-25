a19dee630142fcae1e14194d466c27ee622ffb9d
Jim Counts <jamesrcounts@outlook.com>
2021-04-25 19:34:57 +0000

    Update call sites to use tagged versions

--- projects/root/azure-backend.tf
+++ projects/root/azure-backend.tf
@@ -1,5 +1,5 @@
 module "azure_backend" {
-  source = "github.com/jamesrcounts/devops-governance.git//modules/azure-backend?ref=credentials"
+  source = "github.com/jamesrcounts/devops-governance.git//modules/azure-backend?ref=azure-backend-0.0.2"
 
   location = "centralus"
   required_tags = {
--- projects/root/azure-credential.tf
+++ projects/root/azure-credential.tf
@@ -1,5 +1,5 @@
 module "azure_credentials" {
-  source = "github.com/jamesrcounts/devops-governance.git//modules/azure-credentials?ref=credentials"
+  source = "github.com/jamesrcounts/devops-governance.git//modules/azure-credentials?ref=azure-credentials-0.0.1"
 
   active_password = "primary"
   project         = local.project
