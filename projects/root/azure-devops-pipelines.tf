f40502f679f2133af3cb65de77281dea28e9c39c
Jim Counts <jamesrcounts@outlook.com>
2021-04-25 19:51:29 +0000

    Pass service principal to devops

--- projects/root/azure-devops-pipelines.tf
+++ projects/root/azure-devops-pipelines.tf
@@ -1,5 +1,6 @@
 module "azure_devops_pipelines" {
   source = "github.com/jamesrcounts/devops-governance.git//modules/azure-devops-pipelines?ref=azure-devops"
 
-  project = local.project
+  project           = local.project
+  service_principal = module.azure_credentials.service_principal
 }
