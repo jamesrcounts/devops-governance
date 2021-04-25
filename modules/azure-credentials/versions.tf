16e8c1ec29db135c5d802907580f4d4b16a84a22
Jim Counts <jamesrcounts@outlook.com>
2021-04-25 17:28:06 +0000

    Update outputs

--- modules/azure-credentials/versions.tf
+++ modules/azure-credentials/versions.tf
@@ -4,7 +4,11 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 1.0"
+      version = "~> 1.4"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.56"
     }
     random = {
       source  = "hashicorp/random"
