44363bb4dc222e53e899a2147e1803f546bef638
Jim Counts <jamesrcounts@outlook.com>
2021-04-27 14:04:38 +0000

    Override pipeline branch.

--- projects/root/azure-devops-pipelines.tf
+++ projects/root/azure-devops-pipelines.tf
@@ -1,6 +1,7 @@
 module "azure_devops_pipelines" {
   source = "github.com/jamesrcounts/devops-governance.git//modules/azure-devops-pipelines?ref=azure-devops"
 
+  branch_name       = "azure-devops"
   project           = local.project
   repository        = local.repository
   service_principal = module.azure_credentials.service_principal
