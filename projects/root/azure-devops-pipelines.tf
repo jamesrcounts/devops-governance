463e8b8704878061cfe56bf7faa8613f46a1e9a3
Jim Counts <jamesrcounts@outlook.com>
2021-04-30 13:55:12 +0000

    Wire up URL

--- projects/root/azure-devops-pipelines.tf
+++ projects/root/azure-devops-pipelines.tf
@@ -1,13 +1,17 @@
 module "azure_devops_pipelines" {
   source = "github.com/jamesrcounts/devops-governance.git//modules/azure-devops-pipelines?ref=azure-devops"
 
-  azuredevops_pat   = var.azuredevops_pat
   branch_name       = "azure-devops"
   github_pat        = var.github_pat
   project           = local.project
   repository        = local.repository
   service_principal = module.azure_credentials.service_principal
   yml_path          = "projects/root/azure-pipelines.yml"
+
+  azuredevops = {
+    pat = var.azuredevops_pat
+    url = var.azuredevops_url
+  }
 
   backend = {
     blob_name            = var.key
--- projects/root/variables.tf
+++ projects/root/variables.tf
@@ -1,6 +1,12 @@
 variable "azuredevops_pat" {
-  description = "(Required) An Azure DevOps personal access token. Used to configure an Azure DevOps service connection."
+  description = "(Required) An Azure DevOps personal access token. Used to configure the Azure DevOps Terraform Provider."
   sensitive   = true
+  type        = string
+}
+
+variable "azuredevops_url" {
+  description = "(Required) An Azure DevOps organization URL. Used to configure the Azure DevOps Terraform Provider."
+  type        = string
 }
 
 variable "container_name" {
@@ -11,6 +17,7 @@ variable "container_name" {
 variable "github_pat" {
   description = "(Required) A GitHub personal access token. Used to configure an Azure DevOps service connection."
   sensitive   = true
+  type        = string
 }
 
 variable "key" {
