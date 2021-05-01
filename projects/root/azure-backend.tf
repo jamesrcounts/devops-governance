497bbbc31a1f1cbc8e2d8d93cdfff59aac24d327
Jim Counts <jamesrcounts@outlook.com>
2021-05-01 13:56:12 +0000

    update call site

--- projects/root/azure-backend.tf
+++ projects/root/azure-backend.tf
@@ -1,5 +1,5 @@
 module "azure_backend" {
-  source = "github.com/jamesrcounts/devops-governance.git//modules/azure-backend?ref=azure-backend-0.0.2"
+  source = "github.com/jamesrcounts/devops-governance.git//modules/azure-backend?ref=azure-devops"
 
   location = "centralus"
   required_tags = {
