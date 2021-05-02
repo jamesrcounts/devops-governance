4b554de2f3c1ccd1df0bc807dc87ab575a213d51
Jim Counts <jamesrcounts@outlook.com>
2021-05-02 14:55:45 +0000

    template for backend config

--- modules/azure-backend/backend-config.tf
+++ modules/azure-backend/backend-config.tf
@@ -1,9 +1,14 @@
 resource "local_file" "azurerm_backend_tfvars" {
   filename = "azurerm.backend.tfvars"
-  content = <<EOF
-container_name       = "state"
-key                  = "terraform.tfstate"
-resource_group_name  = "rg-backend-nice-chimp"
-storage_account_name = "sanicechimp"
-EOF
+  content  = data.template_file.backend_config.rendered
+}
+
+data "template_file" "backend_config" {
+  template = file("${path.module}/templates/backend-config.tfvars.hcl")
+  vars = {
+    container_name       = "state"
+    key                  = "terraform.tfstate"
+    resource_group_name  = "rg-backend-nice-chimp"
+    storage_account_name = "sanicechimp"
+  }
 }
---
+++ modules/azure-backend/templates/backend-config.tfvars.hcl
@@ -0,0 +1,4 @@
+container_name       = "state"
+key                  = "terraform.tfstate"
+resource_group_name  = "rg-backend-nice-chimp"
+storage_account_name = "sanicechimp"
