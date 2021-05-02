9193feb704d4e69eb0a32971d09728671656d06c
Jim Counts <jamesrcounts@outlook.com>
2021-05-02 16:45:30 +0000

    sort

--- projects/root/azure-devops-pipelines.tf
+++ projects/root/azure-devops-pipelines.tf
@@ -1,13 +1,13 @@
 module "azure_devops_pipelines" {
   source = "github.com/jamesrcounts/devops-governance.git//modules/azure-devops-pipelines?ref=main"
 
+  backend           = module.azure_backend.backend_config
   branch_name       = "main"
   github_pat        = var.github_pat
   project           = local.project
   repository        = local.repository
   service_principal = module.azure_credentials.service_principal
   yml_path          = "projects/azure-pipelines.yml"
-  backend           = module.azure_backend.backend_config
 
   azuredevops = {
     pat = var.azuredevops_pat
