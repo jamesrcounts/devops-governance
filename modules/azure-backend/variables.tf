6e8dc6adb8846b1dd55b0b4ebb97b2d4e2fa0802
Jim Counts <jamesrcounts@outlook.com>
2021-05-02 16:30:00 +0000

    Sort variables

--- modules/azure-backend/variables.tf
+++ modules/azure-backend/variables.tf
@@ -1,3 +1,8 @@
+variable "backend_config_filename" {
+  default     = null
+  description = "(Optional) When provided a filename this module will write the backend config to that file in tfvars format.  Defaults to null."
+}
+
 variable "location" {
   description = "(Required) Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created."
   type        = string
@@ -15,9 +20,4 @@ variable "optional_tags" {
   default     = {}
   description = "(Optional) Tags to assign to each resource."
   type        = map(string)
-}
-
-variable "backend_config_filename" {
-  default     = null
-  description = "(Optional) When provided a filename this module will write the backend config to that file in tfvars format.  Defaults to null."
 }
