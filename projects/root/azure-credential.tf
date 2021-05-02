fa47d3865fdb9ab4a4af70b15c8f23f6a990d306
Jim Counts <jamesrcounts@outlook.com>
2021-05-02 16:53:05 +0000

    Update callsites to use tags.

--- projects/root/azure-credential.tf
+++ projects/root/azure-credential.tf
@@ -1,5 +1,5 @@
 module "azure_credentials" {
-  source = "github.com/jamesrcounts/devops-governance.git//modules/azure-credentials?ref=main"
+  source = "github.com/jamesrcounts/devops-governance.git//modules/azure-credentials?ref=azure-credentials-0.0.2"
 
   active_password = "primary"
   project         = local.project
