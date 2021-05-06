ecfe99cba307da2cb16abc82691c9f54bbfaa984
Jim Counts <jamesrcounts@outlook.com>
2021-05-06 13:51:21 +0000

    Update callsite for terraform-pipelines rg

--- projects/terraform-pipelines/azure-env.tf
+++ projects/terraform-pipelines/azure-env.tf
@@ -1,5 +1,5 @@
 module "azure_env" {
-  source = "github.com/jamesrcounts/devops-governance.git//modules/azure-resource-group-env?ref=azure-resource-group-env-0.0.1"
+  source = "github.com/jamesrcounts/devops-governance.git//modules/azure-resource-group-env?ref=azure-resource-group-env-0.0.2"
 
   location      = local.location
   required_tags = local.required_tags
