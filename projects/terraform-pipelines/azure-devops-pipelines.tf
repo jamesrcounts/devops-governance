99a3733b95e2655dc6e112bb5b58d0d8e9af0dbc
Jim Counts <jamesrcounts@outlook.com>
2021-05-07 14:41:05 +0000

    Update default branch

--- projects/terraform-pipelines/azure-devops-pipelines.tf
+++ projects/terraform-pipelines/azure-devops-pipelines.tf
@@ -2,7 +2,7 @@ module "azure_devops_pipelines" {
   source = "github.com/jamesrcounts/devops-governance.git//modules/azure-devops-pipelines?ref=azure-devops-pipelines-0.0.3"
 
   backend           = module.azure_backend.backend_config
-  branch_name       = "main"
+  branch_name       = "azureaiconf.6.21"
   github_pat        = var.github_pat
   project           = local.project
   repository        = local.repository
