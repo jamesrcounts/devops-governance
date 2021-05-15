26971bf35c121738475686b4f6b5aa41356761bc
Jim Counts <jamesrcounts@outlook.com>
2021-05-15 20:52:34 +0000

    Fix feature flag for acr

--- modules/azure-devops-pipelines/acr-connection.tf
+++ modules/azure-devops-pipelines/acr-connection.tf
@@ -1,12 +1,17 @@
-resource "azuredevops_serviceendpoint_azurecr" "endpoint" {
-  for_each = toset(var.container_registry[*])
+locals {
+  # Manual check for null because the count may not be known.
+  acrs = var.container_registry == null ? [] : ["acr"]
+}
 
-  azurecr_name              = each.value.acr_name
+resource "azuredevops_serviceendpoint_azurecr" "endpoint" {
+  for_each = toset(local.acrs)
+
+  azurecr_name              = var.container_registry.acr_name
   azurecr_spn_tenantid      = var.service_principal.tenant_id
   azurecr_subscription_id   = var.service_principal.subscription_id
   azurecr_subscription_name = var.service_principal.subscription_name
   description               = "Managed by Terraform"
   project_id                = azuredevops_project.project.id
-  resource_group            = each.value.resource_group_name
+  resource_group            = var.container_registry.resource_group_name
   service_endpoint_name     = "ACR"
 }
