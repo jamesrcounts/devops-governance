bda5281550990c22c50244c5ef71c1b019bc25b6
Jim Counts <jamesrcounts@outlook.com>
2021-05-17 03:25:36 +0000

    Create container for certificates

--- modules/azure-backend/stoarage-container.tf
+++ modules/azure-backend/stoarage-container.tf
@@ -4,3 +4,9 @@ resource "azurerm_storage_container" "state" {
   storage_account_name  = azurerm_storage_account.state.name
 }
 
+resource "azurerm_storage_container" "certs" {
+  container_access_type = "private"
+  name                  = "certs"
+  storage_account_name  = azurerm_storage_account.state.name
+}
+
