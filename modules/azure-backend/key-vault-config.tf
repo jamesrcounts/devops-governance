fdf7a3436ff2c5851039ea5e945e6b177b786171
Jim Counts <jamesrcounts@outlook.com>
2021-05-19 13:29:52 +0000

    Add config keyvault

---
+++ modules/azure-backend/key-vault-config.tf
@@ -0,0 +1,12 @@
+resource "azurerm_key_vault" "config" {
+  enable_rbac_authorization   = true
+  enabled_for_disk_encryption = true
+  location                    = azurerm_resource_group.state.location
+  name                        = "kv-${local.project_id}"
+  purge_protection_enabled    = true
+  resource_group_name         = azurerm_resource_group.state.name
+  sku_name                    = "standard"
+  soft_delete_retention_days  = 7
+  tags                        = local.tags
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+}
