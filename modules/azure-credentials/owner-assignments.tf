98ce742945404134f094a27bdd92aa9681253535
Jim Counts <jamesrcounts@outlook.com>
2021-05-20 13:26:10 +0000

    Assign blob owner to azure credentials

--- modules/azure-credentials/owner-assignments.tf
+++ modules/azure-credentials/owner-assignments.tf
@@ -15,3 +15,12 @@ resource "azurerm_role_assignment" "keyvault_manager" {
   role_definition_name = "Key Vault Administrator"
   scope                = each.value
 }
+
+# The principal can manage all storage blobs in this scope
+resource "azurerm_role_assignment" "blob_owner" {
+  for_each = var.owner_scope
+
+  principal_id         = local.principal_id
+  role_definition_name = "Storage Blob Data Owner"
+  scope                = each.value
+}
