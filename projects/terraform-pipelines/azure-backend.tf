371e7d6609500d82975225119e45003326977ac5
Jim Counts <jamesrcounts@outlook.com>
2021-05-26 14:35:38 +0000

    Update backend version

--- projects/terraform-pipelines/azure-backend.tf
+++ projects/terraform-pipelines/azure-backend.tf
@@ -1,5 +1,5 @@
 module "azure_backend" {
-  source = "github.com/jamesrcounts/devops-governance.git//modules/azure-backend?ref=azure-backend-0.0.4"
+  source = "github.com/jamesrcounts/devops-governance.git//modules/azure-backend?ref=container-pipelines"
 
   location      = local.location
   required_tags = local.required_tags
