0f34002755fd2fa10d68059ee4163d8e29e3ead8
Jim Counts <jamesrcounts@outlook.com>
2021-06-20 16:12:21 +0000

    Disable contianer pipelines

--- projects/container-pipelines.tf
+++ projects/container-pipelines.tf
@@ -1,9 +1,9 @@
-module "container_pipelines" {
-  source = "./container-pipelines"
+// module "container_pipelines" {
+//   source = "./container-pipelines"
 
-  aad_script_name = var.container_pipelines_aad_script_name
-  aws             = local.aws
-  github_pat      = var.github_pat
-  project         = "container-pipelines"
-  repository      = "jamesrcounts/phippyandfriends"
-}
+//   aad_script_name = var.container_pipelines_aad_script_name
+//   aws             = local.aws
+//   github_pat      = var.github_pat
+//   project         = "container-pipelines"
+//   repository      = "jamesrcounts/phippyandfriends"
+// }
