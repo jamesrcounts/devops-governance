0226f1d45b852b29690f7b5c73f406e14b454909
Jim Counts <jamesrcounts@outlook.com>
2021-06-20 16:10:07 +0000

    Remove extra resources from aks-istio

--- projects/aks-istio/azure-env.tf
+++ projects/aks-istio/azure-env.tf
@@ -3,4 +3,6 @@ module "azure_env" {
 
   location      = local.location
   required_tags = local.required_tags
+
+  optional_tags = { backend_instance_id = module.azure_backend.resource_group.tags["instance_id"] }
 }
--- projects/aks-istio/variables.tf
+++ projects/aks-istio/variables.tf
@@ -1,29 +1,14 @@
-// variable "aad_script_name" {
-//   default     = null
-//   description = "(Optional) When provided a filename this module will write a powershell script to assign AAD roles to these credentials.  Defaults to null."
+// variable "github_pat" {
+//   description = "(Required) A GitHub personal access token. Used to configure an Azure DevOps service connection."
+//   sensitive   = true
 //   type        = string
 // }
 
-// variable "aws" {
-//   description = "(Required) Specifies the credentials for the AWS Terraform provider."
-//   sensitive   = true
-//   type = object({
-//     id  = string
-//     key = string
-//   })
+// variable "generate_scripts" {
+//   default     = false
+//   description = "(Optional) A flag to indicate whether to generate run-once scripts."
+//   type        = bool
 // }
-
-variable "github_pat" {
-  description = "(Required) A GitHub personal access token. Used to configure an Azure DevOps service connection."
-  sensitive   = true
-  type        = string
-}
-
-variable "generate_scripts" {
-  default     = false
-  description = "(Optional) A flag to indicate whether to generate run-once scripts."
-  type        = bool
-}
 
 variable "project" {
   description = "(Required) The project name."
