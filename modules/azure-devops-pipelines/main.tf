c63d6d55530d4b179d95ec3b94c12a4439a797d6
Jim Counts <jamesrcounts@outlook.com>
2021-07-05 21:50:57 +0000

    Add 1.0.1

--- modules/azure-devops-pipelines/main.tf
+++ modules/azure-devops-pipelines/main.tf
@@ -4,6 +4,7 @@ locals {
     "0.15.3" = "5ce5834fd74e3368ad7bdaac847f973e66e61acae469ee86b88da4c6d9f933d4"
     "0.15.4" = "ddf9b409599b8c3b44d4e7c080da9a106befc1ff9e53b57364622720114e325c"
     "1.0.0"  = "8be33cc3be8089019d95eb8f546f35d41926e7c1e5deff15792e969dde573eb5"
+    "1.0.1"  = "da94657593636c8d35a96e4041136435ff58bb0061245b7d0f82db4a7728cef3"
   }
 
   pipelines = var.pipelines != null ? var.pipelines : { "infrastructure" = "azure-pipelines.yml" }
