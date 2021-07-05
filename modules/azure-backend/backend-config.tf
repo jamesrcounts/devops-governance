6c579abe27384b086ae9aadb4ff3afafcf62132e
Jim Counts <jamesrcounts@outlook.com>
2021-07-05 18:44:41 +0000

    Add backend config to keyvault

--- modules/azure-backend/backend-config.tf
+++ modules/azure-backend/backend-config.tf
@@ -1,8 +1,5 @@
-resource "local_file" "azurerm_backend_tfvars" {
-  for_each = toset(var.backend_config_filename[*])
-
-  filename = each.key
-  content = templatefile(
+locals{
+  backend_config = templatefile(
     "${path.module}/templates/backend-config.tfvars.hcl",
     {
       container_name       = azurerm_storage_container.state.name
@@ -11,4 +8,17 @@ resource "local_file" "azurerm_backend_tfvars" {
       storage_account_name = azurerm_storage_account.state.name
     }
   )
+}
+
+resource "local_file" "azurerm_backend_tfvars" {
+  for_each = toset(var.backend_config_filename[*])
+
+  content = local.backend_config
+  filename = each.key
+}
+
+resource "azurerm_key_vault_secret" "backend_config" {
+  key_vault_id = module.azure_backend.key_vault.id
+  name         = "azurerm-backend-tfvars"
+  value        = local.backend_config
 }
