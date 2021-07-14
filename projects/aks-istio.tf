080b3a34cb083b336eb45303c5dd89a684acb703
Jim Counts <jamesrcounts@outlook.com>
2021-07-14 14:35:53 +0000

    Enable AKS Istio

--- projects/aks-istio.tf
+++ projects/aks-istio.tf
@@ -1,8 +1,8 @@
-// module "aks_istio" {
-//   source = "./aks-istio"
+module "aks_istio" {
+  source = "./aks-istio"
 
-//   github_pat = var.github_pat
-//   aws        = local.aws
-//   project    = "aks-istio"
-//   repository = "jamesrcounts/aks-istio"
-// }
+  github_pat = var.github_pat
+  aws        = local.aws
+  project    = "aks-istio"
+  repository = "jamesrcounts/aks-istio"
+}
