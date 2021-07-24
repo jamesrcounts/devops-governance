5a8ed2836fc8738573e94e7c1b858c760ad94c52
Jim Counts <jamesrcounts@outlook.com>
2021-07-24 17:26:13 +0000

    Remove stg provider

--- projects/providers.tf
+++ projects/providers.tf
@@ -1,9 +1,4 @@
 provider "acme" {
-  alias      = "stg"
-  server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
-}
-
-provider "acme" {
   server_url = "https://acme-v02.api.letsencrypt.org/directory"
 }
 
