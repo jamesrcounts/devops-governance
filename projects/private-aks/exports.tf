357351ee23f31a562817d22b73c9ba4880228a50
Jim Counts <jamesrcounts@outlook.com>
2021-08-18 13:58:58 +0000

    add Aks administrators group

--- projects/private-aks/exports.tf
+++ projects/private-aks/exports.tf
@@ -1,6 +1,7 @@
 resource "azurerm_key_vault_secret" "exports" {
   for_each = {
-    msi-resource-id = module.aks_cluster_identity.msi_resource_id
+    admin-group-object-id = azuread_group.aks_administrators.object_id
+    msi-resource-id       = module.aks_cluster_identity.msi_resource_id
   }
 
   key_vault_id = module.azure_backend.key_vault.id
