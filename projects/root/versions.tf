2b41e1f8758b319eeabb4c211d173e407711ecfd
Jim Counts <jamesrcounts@outlook.com>
2021-08-18 13:51:44 +0000

    Declare dependency versions

--- projects/root/versions.tf
+++ projects/root/versions.tf
@@ -4,5 +4,13 @@ terraform {
       source  = "microsoft/azuredevops"
       version = "~> 0"
     }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0"
+    }
   }
 }
