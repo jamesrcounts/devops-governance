feae30c2504a59475705e108fe6fc539c6bda59b
Jim Counts <jamesrcounts@outlook.com>
2021-08-24 14:43:29 +0000

    Update default branch

--- projects/root/azure-devops-pipelines.tf
+++ projects/root/azure-devops-pipelines.tf
@@ -2,7 +2,7 @@ module "azure_devops_pipelines" {
   source = "github.com/jamesrcounts/devops-governance.git//modules/azure-devops-pipelines?ref=azure-devops-pipelines-0.0.6"
 
   backend           = module.azure_backend.backend_config
-  branch_name       = "aks-istio"
+  branch_name       = "main"
   github_pat        = var.github_pat
   project           = local.project
   repository        = local.repository
