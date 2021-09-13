794fd6261f469bbeb9e37a9e7c0b44e7ffac814f
Jim Counts <jamesrcounts@outlook.com>
2021-09-13 00:37:29 +0000

    Enable AGW VM

--- projects/agw-vm.tf
+++ projects/agw-vm.tf
@@ -1,7 +1,7 @@
-// module "agw_vm" {
-//   source = "./agw-vm"
+module "agw_vm" {
+  source = "./agw-vm"
 
-//   github_pat = var.github_pat
-//   project    = "agw-vm"
-//   repository = "jamesrcounts/agw-vm"
-// }
+  github_pat = var.github_pat
+  project    = "agw-vm"
+  repository = "jamesrcounts/agw-vm"
+}
