28970a24b2fc89ab8b47764c4361cf86165e086a
Jim Counts <jamesrcounts@outlook.com>
2021-11-27 14:52:07 +0000

    Pin root module version

--- projects/root/azure-devops-pipelines.tf
+++ projects/root/azure-devops-pipelines.tf
@@ -1,5 +1,5 @@
 module "azure_devops_pipelines" {
-  source = "github.com/jamesrcounts/devops-governance.git//modules/azure-devops-pipelines?ref=main"
+  source = "github.com/jamesrcounts/devops-governance.git//modules/azure-devops-pipelines?ref=azure-devops-pipelines-0.0.7"
 
   backend           = module.azure_backend.backend_config
   branch_name       = "main"
