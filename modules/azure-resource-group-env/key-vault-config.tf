5ae933660c4ae8e20eb565f5eed09163f38f401d
Jim Counts <jamesrcounts@outlook.com>
2021-11-28 03:53:12 +0000

    Disable injection of instance_id

--- modules/azure-resource-group-env/key-vault-config.tf
+++ modules/azure-resource-group-env/key-vault-config.tf
@@ -1,7 +1,7 @@
-resource "azurerm_key_vault_secret" "config" {
-  key_vault_id = var.config_key_vault_id
-  name         = "${local.tags["purpose"]}-instance-id"
-  value        = local.instance_id
+# resource "azurerm_key_vault_secret" "config" {
+#   key_vault_id = var.config_key_vault_id
+#   name         = "${local.tags["purpose"]}-instance-id"
+#   value        = local.instance_id
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
