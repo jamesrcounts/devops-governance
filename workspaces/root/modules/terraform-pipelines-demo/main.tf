5e5d4f7b910818dab864f719c743792f878f9e06
Jim Counts <jamesrcounts@outlook.com>
2022-12-06 00:01:43 +0000

    Remove empty module

--- workspaces/root/modules/terraform-pipelines-demo/main.tf
+++ workspaces/root/modules/terraform-pipelines-demo/main.tf
@@ -1,3 +1,17 @@
+/**
+ * # Terraform Pipelines Workspace
+ *
+ * Workspaces connect Terraform Cloud to a GitHub Repository and an Azure
+ * Environment.
+ *
+ * However, other combinations _are_ possible!  For example you could use GCP
+ * instead, or Azure DevOps.  But you would have to write your own module for
+ * that ;).
+ *
+ * This module implements the specific pattern:
+ *  GitHub <=> Terraform Cloud <=> Azure
+ */
+
 locals {
   workspace_directory = "infrastructure"
   workspace_name      = "terraform-pipelines-${local.namespace}"
